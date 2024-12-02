be79465ca73017d054d868ca8492a51324de53fb
maxgrotstein <maxgrotstein@gmail.com>
2024-12-02 17:52:44 -0500

    Updated routes

---
+++ api/backend/cooperate/review_routes.py
@@ -0,0 +1,130 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+#------------------------------------------------------------
+# Create a new Blueprint object, which is a collection of 
+# routes.
+reviews = Blueprint('reviews', __name__)
+
+#------------------------------------------------------------
+# TODO
+@reviews.route('/reviews', methods=['GET'])
+def get_reviews():
+    query = '''
+        SELECT  StudentID, 
+                FirstName, 
+                LastName, 
+                Major, 
+                GPA,
+                HomeCollege,
+                Email 
+        FROM Students
+    '''
+    
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # fetch all the data from the cursor
+    # The cursor will return the data as a 
+    # Python Dictionary
+    theData = cursor.fetchall()
+
+    # Create a HTTP Response object and add results of the query to it
+    # after "jasonify"-ing it.
+    response = make_response(jsonify(theData))
+    # set the proper HTTP Status code of 200 (meaning all good)
+    response.status_code = 200
+    # send the response back to the client
+    return response
+
+#------------------------------------------------------------
+# TODO
+@reviews.route('/reviews/<studentID>', methods=['GET'])
+def get_student_reviews(studentID):
+    query = f'''
+        SELECT *
+        FROM Reviews
+        WHERE StudentID = {str(studentID)}
+    '''
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # fetch all the data from the cursor
+    # The cursor will return the data as a 
+    # Python Dictionary
+    theData = cursor.fetchall()
+
+    # Create a HTTP Response object and add results of the query to it
+    # after "jasonify"-ing it.
+    response = make_response(jsonify(theData))
+    # set the proper HTTP Status code of 200 (meaning all good)
+    response.status_code = 200
+    # send the response back to the client
+    return response
+
+#------------------------------------------------------------
+# TODO
+@reviews.route('/reviews/<studentID>/<positionID>', methods=['GET'])
+def get_student_reviews(studentID, positionID):
+    query = f'''
+        SELECT *
+        FROM Reviews
+        WHERE StudentID = {str(studentID)}
+        AND PositionID = {str(positionID)}
+    '''
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # fetch all the data from the cursor
+    # The cursor will return the data as a 
+    # Python Dictionary
+    theData = cursor.fetchall()
+
+    # Create a HTTP Response object and add results of the query to it
+    # after "jasonify"-ing it.
+    response = make_response(jsonify(theData))
+    # set the proper HTTP Status code of 200 (meaning all good)
+    response.status_code = 200
+    # send the response back to the client
+    return response
+
+#------------------------------------------------------------
+# TODO
+@reviews.route('/reviews/<studentID>/<positionID>', methods=['POST'])
+def get_student_reviews(studentID, positionID):
+    query = f'''
+        INSERT INTO Reviews (StudentID, Date, Culture, Satisfaction, Compensation,
+          LearningOpportunity, WorkLifeBalance, Summary, PositionID)
+        VALUES ()
+    '''
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # fetch all the data from the cursor
+    # The cursor will return the data as a 
+    # Python Dictionary
+    theData = cursor.fetchall()
+
+    # Create a HTTP Response object and add results of the query to it
+    # after "jasonify"-ing it.
+    response = make_response(jsonify(theData))
+    # set the proper HTTP Status code of 200 (meaning all good)
+    response.status_code = 200
+    # send the response back to the client
+    return response
