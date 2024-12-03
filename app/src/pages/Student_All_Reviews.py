3729e3885cf1191673b0521b793312e18492cc11
maxgrotstein <maxgrotstein@gmail.com>
2024-12-02 21:00:40 -0500

    Actual Reviews

--- app/src/pages/Student_All_Reviews.py
+++ app/src/pages/Student_All_Reviews.py
@@ -5,3 +5,14 @@ st.set_page_config(page_title="All Reviews", layout="wide")
 
 # Header
 st.markdown("# All Reviews")
+
+data = {} 
+
+try:
+    data = requests.get('http://api:4000/r/reviews').json()
+except:
+    st.write("**Important**: Could not connect to sample api, so using dummy data.")
+    data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
+
+st.dataframe(data)
+
